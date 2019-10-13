1be2958d8a2b4f3657318687bb8c91782b67b7ff
Janel <jannel@example.com>
2019-10-13 17:02:35 +0600

    first commit

---
+++ src/sweeper/Ranges.java
@@ -0,0 +1,12 @@
+package sweeper;
+
+public class Ranges {
+
+    private static Coord size;
+    public static void  setSize(Coord size){
+        size = size;
+    }
+    public static Coord getSize() {
+        return size;
+    }
+}
